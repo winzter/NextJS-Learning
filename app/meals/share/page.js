22aff40bb83da36e33d015a44a12fb00cf50f82b
Phakin Neamson <phakin.neamson@gmail.com>
2024-07-22 22:56:22 +0700

    Init

---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ShareMealPage() {
+  return (
+    <h1>Share Meal</h1>
+  )
+}
---
+++ components/main-header/main-header.js
@@ -0,0 +1,36 @@
+
+import Link from 'next/link'
+import Image from 'next/image'
+import logoImg from '@/assets/logo.png' // for <img> , It is an object that has "src" as an attribute to keep the path of img
+import classes from './main-header.module.css'
+import MainHeaderBackground from './main-header-background'
+import NavLink from './nav-link'
+
+export default function MainHeader() {
+  return (
+    <>
+        <MainHeaderBackground/>
+        <header className={classes['header']}>
+            <Link className={classes.logo} href="/">
+                <Image priority src={logoImg} alt="A plate with food on it"/>
+                NextLevel Food
+            </Link>
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                       <NavLink href='/meals'>
+                            Browse Meals
+                       </NavLink>
+                    </li>
+                    <li>
+                        <NavLink href='/community'>
+                            Foodies Community
+                       </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </>
+    
+  )
+}
